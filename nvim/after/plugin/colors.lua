0e3eaeab4547d1f944229fc0d80b0074ee98cdb5
nkdem <dev@nkdem.net>
2024-01-07 15:18:56 +0000

    initial commit

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,8 @@
+require("tokyonight").setup({
+  style = 'night'
+})
+
+vim.cmd.colorscheme("tokyonight")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
