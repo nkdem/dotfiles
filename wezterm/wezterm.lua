b16550f7cbf2c63b66571cf587fc6fb2acb4dd62
nkdem <dev@nkdem.net>
2024-01-08 01:01:43 +0000

    add keybindings to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This table will hold the configuration.
 local config = {}
@@ -15,6 +16,41 @@ end
 -- For example, changing the color scheme:
 config.color_scheme = 'Tokyo Night'
 config.font_size = 21.5
+config.leader = { key="`", mods="NONE", timeout_milliseconds=1000 }
+config.keys = {
+    {key="a",  mods="LEADER", action=act{SendString="`"}},
+    {key='"',  mods="LEADER", action=act{SplitVertical={domain="CurrentPaneDomain"}} },
+    {key="%", mods="LEADER", action=act.SplitHorizontal{domain="CurrentPaneDomain"}},
+    {key="z" , mods="LEADER", action="TogglePaneZoomState" },
+    {key="c" , mods="LEADER", action=act{SpawnTab="CurrentPaneDomain"}},
 
+    {key="h", mods="LEADER", action=act.ActivatePaneDirection("Left")},
+    {key="j", mods="LEADER", action=act.ActivatePaneDirection("Down")},
+    {key="k", mods="LEADER", action=act.ActivatePaneDirection("Up")},
+    {key="l", mods="LEADER", action=act.ActivatePaneDirection("Right")},
+
+    {key="H", mods="LEADER", action=act{AdjustPaneSize={"Left", 5}}},
+    {key="J", mods="LEADER", action=act{AdjustPaneSize={"Down", 5}}},
+    {key="K", mods="LEADER", action=act{AdjustPaneSize={"Up", 5}}},
+    {key="L", mods="LEADER", action=act{AdjustPaneSize={"Right", 5}}},
+
+    {key="`", mods="LEADER", action=act.ActivateLastTab},
+    {key=" ", mods="LEADER", action=act.ActivateTabRelative(1)},
+    {key="1", mods="LEADER", action=act{ActivateTab=0}},
+    {key="2", mods="LEADER", action=act{ActivateTab=1}},
+    {key="3", mods="LEADER", action=act{ActivateTab=2}},
+    {key="4", mods="LEADER", action=act{ActivateTab=3}},
+    {key="5", mods="LEADER", action=act{ActivateTab=4}},
+    {key="6", mods="LEADER", action=act{ActivateTab=5}},
+    {key="7", mods="LEADER", action=act{ActivateTab=6}},
+    {key="8", mods="LEADER", action=act{ActivateTab=7}},
+    {key="9", mods="LEADER", action=act{ActivateTab=8}},
+    {key="x", mods="LEADER", action=act{CloseCurrentPane={confirm=true}}},
+
+    -- Activate Copy Mode
+    {key="[", mods="LEADER", action=act.ActivateCopyMode},
+    -- Paste from Copy Mode
+    {key="]", mods="LEADER", action=act.PasteFrom("PrimarySelection")},
+}
 -- and finally, return the configuration to wezterm
 return config
