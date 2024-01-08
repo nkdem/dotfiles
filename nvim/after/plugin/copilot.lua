d7fd0a7909d52c0c46200f765fe746fe01ba0eb2
nkdem <dev@nkdem.net>
2024-01-08 11:36:10 +0000

    Add copilot word completion; format lua files

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,31 +1,31 @@
-require('copilot').setup({
-  panel = {
-    enabled = false,
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    debounce = 75,
-    keymap = {
-      accept = "<M-l>",
-      accept_word = false,
-      accept_line = false,
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 18.x
-  server_opts_overrides = {},
+require("copilot").setup({
+	panel = {
+		enabled = false,
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		debounce = 75,
+		keymap = {
+			accept = "<M-l>",
+			accept_word = "<M-S-l>",
+			accept_line = false,
+			next = false,
+			prev = false,
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		yaml = false,
+		markdown = false,
+		help = false,
+		gitcommit = true,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
+	copilot_node_command = "node", -- Node.js version must be > 18.x
+	server_opts_overrides = {},
 })
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 -- This table will hold the configuration.
@@ -8,49 +8,49 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = "Tokyo Night"
 config.font_size = 21.5
-config.leader = { key="`", mods="NONE", timeout_milliseconds=1000 }
+config.leader = { key = "§", mods = "NONE", timeout_milliseconds = 1000 }
 config.keys = {
-    {key="a",  mods="LEADER", action=act{SendString="`"}},
-    {key='"',  mods="LEADER", action=act{SplitVertical={domain="CurrentPaneDomain"}} },
-    {key="%", mods="LEADER", action=act.SplitHorizontal{domain="CurrentPaneDomain"}},
-    {key="z" , mods="LEADER", action="TogglePaneZoomState" },
-    {key="c" , mods="LEADER", action=act{SpawnTab="CurrentPaneDomain"}},
+	{ key = "a", mods = "LEADER", action = act({ SendString = "`" }) },
+	{ key = '"', mods = "LEADER", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = "%", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "c", mods = "LEADER", action = act({ SpawnTab = "CurrentPaneDomain" }) },
 
-    {key="h", mods="LEADER", action=act.ActivatePaneDirection("Left")},
-    {key="j", mods="LEADER", action=act.ActivatePaneDirection("Down")},
-    {key="k", mods="LEADER", action=act.ActivatePaneDirection("Up")},
-    {key="l", mods="LEADER", action=act.ActivatePaneDirection("Right")},
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
 
-    {key="H", mods="LEADER", action=act{AdjustPaneSize={"Left", 5}}},
-    {key="J", mods="LEADER", action=act{AdjustPaneSize={"Down", 5}}},
-    {key="K", mods="LEADER", action=act{AdjustPaneSize={"Up", 5}}},
-    {key="L", mods="LEADER", action=act{AdjustPaneSize={"Right", 5}}},
+	{ key = "H", mods = "LEADER", action = act({ AdjustPaneSize = { "Left", 5 } }) },
+	{ key = "J", mods = "LEADER", action = act({ AdjustPaneSize = { "Down", 5 } }) },
+	{ key = "K", mods = "LEADER", action = act({ AdjustPaneSize = { "Up", 5 } }) },
+	{ key = "L", mods = "LEADER", action = act({ AdjustPaneSize = { "Right", 5 } }) },
 
-    {key="`", mods="LEADER", action=act.ActivateLastTab},
-    {key=" ", mods="LEADER", action=act.ActivateTabRelative(1)},
-    {key="1", mods="LEADER", action=act{ActivateTab=0}},
-    {key="2", mods="LEADER", action=act{ActivateTab=1}},
-    {key="3", mods="LEADER", action=act{ActivateTab=2}},
-    {key="4", mods="LEADER", action=act{ActivateTab=3}},
-    {key="5", mods="LEADER", action=act{ActivateTab=4}},
-    {key="6", mods="LEADER", action=act{ActivateTab=5}},
-    {key="7", mods="LEADER", action=act{ActivateTab=6}},
-    {key="8", mods="LEADER", action=act{ActivateTab=7}},
-    {key="9", mods="LEADER", action=act{ActivateTab=8}},
-    {key="x", mods="LEADER", action=act{CloseCurrentPane={confirm=true}}},
+	{ key = "`", mods = "LEADER", action = act.ActivateLastTab },
+	{ key = " ", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "1", mods = "LEADER", action = act({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER", action = act({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER", action = act({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER", action = act({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER", action = act({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER", action = act({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER", action = act({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER", action = act({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER", action = act({ ActivateTab = 8 }) },
+	{ key = "x", mods = "LEADER", action = act({ CloseCurrentPane = { confirm = true } }) },
 
-    -- Activate Copy Mode
-    {key="[", mods="LEADER", action=act.ActivateCopyMode},
-    -- Paste from Copy Mode
-    {key="]", mods="LEADER", action=act.PasteFrom("PrimarySelection")},
+	-- Activate Copy Mode
+	{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
+	-- Paste from Copy Mode
+	{ key = "]", mods = "LEADER", action = act.PasteFrom("PrimarySelection") },
 }
 -- and finally, return the configuration to wezterm
 return config
